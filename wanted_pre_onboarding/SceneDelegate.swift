79f4cb43fb400b51d00d6f727aa377e45bbad2db
prettyturtle <unifconti@gmail.com>
2022-06-12 19:39:09 +0900

    First Commit, UI Code-base

---
+++ wanted_pre_onboarding/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  wanted_pre_onboarding
+//
+//  Created by yc on 2022/06/12.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        let rootViewController = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by yc on 2022/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
